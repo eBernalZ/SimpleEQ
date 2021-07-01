2cda142f93558cd36c96233f0a4ae564c18eef7e
Emilio Bernal <ebernalzubieta@gmail.com>
2021-07-01 16:56:46 -0500

    visualization

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -107,6 +107,9 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     rightChain.prepare(spec);
     
     updateFilters();
+    
+    leftChannelFifo.prepare(samplesPerBlock);
+    rightChannelFifo.prepare(samplesPerBlock);
 }
 
 void SimpleEQAudioProcessor::releaseResources()
@@ -169,6 +172,8 @@ void SimpleEQAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce
     leftChain.process(leftContext);
     rightChain.process(rightContext);
     
+    leftChannelFifo.update(buffer);
+    rightChannelFifo.update(buffer);
 }
 
 //==============================================================================
