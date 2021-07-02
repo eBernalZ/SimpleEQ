d58be8f66c24d64666772ffd9c02468fcdb653b8
Emilio Bernal <ebernalzubieta@gmail.com>
2021-07-02 10:31:08 -0500

    fixed positioning

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -94,7 +94,7 @@ struct AnalyzerPathGenerator {
             jassert( !std::isnan(y) && !std::isinf(y));
             if( !std::isnan(y) && !std::isinf(y)) {
                 auto binFreq = binNum * binWidth;
-                auto normalizedBinX = juce::mapFromLog10(binFreq, 1.f, 20000.f);
+                auto normalizedBinX = juce::mapFromLog10(binFreq, 20.f, 20000.f);
                 int binX = std::floor(normalizedBinX * width);
                 p.lineTo(binX,y);
             }
